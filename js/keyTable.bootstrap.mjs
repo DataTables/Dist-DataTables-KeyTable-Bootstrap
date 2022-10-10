824188c4fb9ca96f5fbc3d70ecd117eb8754f6c6
Allan Jardine <allan@datatables.net>
2022-10-10 19:49:38 +0000

    Add support for mjs files (ES Modules)

---
+++ js/keyTable.bootstrap.mjs
@@ -0,0 +1,13 @@
+
+/*! Bootstrap 3 styling wrapper for KeyTable
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-bs';
+import DataTable from 'datatables.net-keytable';
+
+
+
+
+export default DataTable;
