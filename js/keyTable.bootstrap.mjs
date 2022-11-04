86f8886fba93812ba032a8843f8fbc87a210b0ef
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 17:06:42 +0000

    2de7c4a50f823d633cecea052206234aa575c1ba
    Sync to source repo @2de7c4a50f823d633cecea052206234aa575c1ba

--- js/keyTable.bootstrap.mjs
+++ js/keyTable.bootstrap.mjs
@@ -1,45 +1,13 @@
 
 /*! Bootstrap 3 styling wrapper for KeyTable
- * ©2018 SpryMedia Ltd - datatables.net/license
+ * © SpryMedia Ltd - datatables.net/license
  */
 
-import DataTable from '';
+import $ from 'jquery';
+import DataTable from 'datatables.net-bs';
+import DataTable from 'datatables.net-keytable';
 
 
 
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net-bs', 'datatables.net-keytable'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		module.exports = function (root, $) {
-			if ( ! root ) {
-				root = window;
-			}
-
-			if ( ! $ || ! $.fn.dataTable ) {
-				$ = require('datatables.net-bs')(root, $).$;
-			}
-
-			if ( ! $.fn.dataTable.KeyTable ) {
-				require('datatables.net-keytable')(root, $);
-			}
-
-			return factory( $, root, root.document );
-		};
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document, undefined ) {
-
-return $.fn.dataTable;
-
-}));
 
 export default DataTable;
